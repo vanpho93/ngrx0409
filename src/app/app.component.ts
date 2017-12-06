9bb7f3835f208126572b0e9835eba9cdf060962e
Van Pho Nguyen <vanpho02@gmail.com>
2017-12-06 18:49:50 +0700

    reset

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,34 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
+import { CHANGE, RESET } from './ngrx/counter';
+
+interface AppState {
+  counter: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   counter: Observable<number>;
 
-  constructor(private store: Store<any>) {
+  constructor(private store: Store<AppState>) {
     this.counter = this.store.select('counter');
+  }
+
+  add() {
+    this.store.dispatch({ type: CHANGE, changeValue: 1 });
+  }
+
+  sub() {
+    this.store.dispatch({ type: CHANGE, changeValue: -1 });
+  }
+
+  reset() {
+    this.store.dispatch({ type: RESET });
   }
 }
--- src/app/ngrx/counter.ts
+++ src/app/ngrx/counter.ts
@@ -2,8 +2,13 @@ import { Action } from '@ngrx/store';
 // Quan ly trang thai cua ung dung
 const defaultValue = 10;
 
-export const counterReducer = (state: number = defaultValue, action: Action) => {
-    if (action.type === 'ADD') return state + 1;
-    if (action.type === 'SUB') return state - 1;
+export const CHANGE = 'CHANGE';
+export const RESET = 'RESET';
+
+export const counterReducer = (state: number = defaultValue, action) => {
+    // if (action.type === 'ADD') return state + 1;
+    // if (action.type === 'SUB') return state - 1;
+    if (action.type === CHANGE) return state + action.changeValue;
+    if (action.type === RESET) return defaultValue;
     return state;
 };
